328d7938b08f185817dfa47db081c5f8fe2f4b70
The2beats4hits <2beats4hits@gmail.com>
2016-09-20 18:19:38 +0200

    Static dir changes

--- example/settings.py
+++ example/settings.py
@@ -122,11 +122,10 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Auth settings
 
 LOGIN_URL = "/login/"
